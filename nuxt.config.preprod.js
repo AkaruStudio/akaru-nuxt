8a0888749006b756f9b2efee0bb1730ad158853c
Quentin Neyraud <quentin.neyraud@gmail.com>
2019-02-25 10:13:01 +0100

    add sitemap generation on preprod and prod build

--- nuxt.config.preprod.js
+++ nuxt.config.preprod.js
@@ -1,1 +1,9 @@
-module.exports = Object.assign({}, require('./nuxt.config'))
+module.exports = Object.assign({}, require('./nuxt.config'), {
+  sitemap: {
+    path: '/sitemap.xml',
+    hostname: 'clients.akaru.fr/my-project',
+    generate: true,
+    // additional routes
+    routes: () => []
+  },
+})
--- nuxt.config.prod.js
+++ nuxt.config.prod.js
@@ -1,1 +1,9 @@
-module.exports = Object.assign({}, require('./nuxt.config'))
+module.exports = Object.assign({}, require('./nuxt.config'), {
+  sitemap: {
+    path: '/sitemap.xml',
+    hostname: 'my-project.fr',
+    generate: true,
+    // additional routes
+    routes: () => []
+  },
+})
